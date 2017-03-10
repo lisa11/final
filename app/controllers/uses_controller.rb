e99b85c042e32c4aaf3372a8c687aa69f8418ecd
Raghu Betina <raghu@firstdraft.com>
2017-03-10 17:44:10 +0000

    Only use user can modify use

--- app/controllers/uses_controller.rb
+++ app/controllers/uses_controller.rb
@@ -1,4 +1,14 @@
 class UsesController < ApplicationController
+  before_action :current_user_must_be_use_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_use_user
+    use = Use.find(params[:id])
+
+    unless current_user == use.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @uses = Use.all
 
