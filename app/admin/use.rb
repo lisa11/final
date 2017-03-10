7121690cfb38b970617dd7f288971f03de91fdc2
Raghu Betina <raghu@firstdraft.com>
2017-03-10 17:44:07 +0000

    Whitelist columns in Use for ActiveAdmin

--- app/admin/use.rb
+++ app/admin/use.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Use do
 
+ permit_params :purchase_id, :user_id, :date
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
